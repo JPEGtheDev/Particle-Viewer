d77c73b8f55045823db2cd1a5b57d992efb5763b
Jonathan Petz <JPEGtheDev@users.noreply.github.com>
2015-09-23 16:01:46 -0500

    Update gccBuild.sh
    trying to get a statically linked SDL to remove a good chunk of testing time that could be used for other people on travis-ci

--- scripts/gccBuild.sh
+++ scripts/gccBuild.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 #gcc build script
-gcc ./src/main.cpp -o ./bin/viewer  -lSDL2 -lGLEW -I. -g -O0 -Wall;
+gcc ./src/main.cpp -o ./bin/viewer -lGLEW -I. -g -O0 -Wall ./SDL/build/build/libSDL2main.a;
