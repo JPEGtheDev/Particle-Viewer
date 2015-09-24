b3a60292739025c78f59f832267a645810dd235f
Jonathan Petz <JPEGtheDev@users.noreply.github.com>
2015-09-24 18:51:03 -0500

    Update g++Build.sh

--- scripts/g++Build.sh
+++ scripts/g++Build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 #gcc build script
-g++ ./src/main.cpp -std=c++11 -o ./bin/viewer -ldl -pthread  -I. -g -O0  ./src/deps/lib/libSDL2.a ./src/deps/lib/libSDL2main.a  -lGLEW;
+g++ ./src/main.cpp -o ./bin/viewer -ldl -pthread  -I. -g -O0  ./src/deps/lib/libSDL2.a ./src/deps/lib/libSDL2main.a  -lGLEW;
 ./bin/viewer
