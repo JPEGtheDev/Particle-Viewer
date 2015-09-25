2ef2189634636918a70a0d1b1bf2b9414839ffeb
Jonathan Petz <JPEGtheDev@users.noreply.github.com>
2015-09-24 19:13:15 -0500

    Update linuxPre.sh

--- scripts/linuxPre.sh
+++ scripts/linuxPre.sh
@@ -3,7 +3,7 @@
     cd SDL;
     mkdir build;
     cd build;
-    ../configure --prefix="../../SDL2_Build";
+    ../configure --prefix=/home/travis/build/JPEGtheDev/Particle-Viewer/SDL2_Build
     pwd;
     make;
     cd /home/travis/build/JPEGtheDev/Particle-Viewer;
