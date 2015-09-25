bdc4d5019ed20b51d55aeccd62f26840a4d01dd3
Jonathan Petz <JPEGtheDev@users.noreply.github.com>
2015-09-24 19:19:36 -0500

    Update g++LinuxBuild.sh

--- scripts/g++LinuxBuild.sh
+++ scripts/g++LinuxBuild.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 #gcc build script
-g++ ./src/main.cpp -o ./bin/viewer -ldl -pthread  -I. -g -O0  ./src/deps/lib/libSDL2.a ./src/deps/lib/libSDL2main.a  -lGLEW;
+g++ ./src/main.cpp -o ./bin/viewer -ldl -pthread  -I. -g -O0  /home/travis/build/JPEGtheDev/Particle-Viewer/SDL2_Build/lib/libSDL2.a /home/travis/build/JPEGtheDev/Particle-Viewer/SDL2_Build/lib/libSDL2main.a -I/home/travis/build/JPEGtheDev/Particle-Viewer/SDL2_Build/include  -lGLEW;
