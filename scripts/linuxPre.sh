97d551a60ac450d026d6a107086ffe615749af8a
Jonathan Petz <jpegthedev@gmail.com>
2015-09-24 20:58:38 -0500

    Removed SDL2
    Removed the library for SDL2, and I’m testing to see if the mods that I
    made will work.

--- scripts/linuxPre.sh
+++ scripts/linuxPre.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env sh
-    hg clone https://hg.libsdl.org/SDL SDL;
-    cd SDL;
-    mkdir build;
-    cd build;
-    ../configure --prefix=/home/travis/build/JPEGtheDev/Particle-Viewer/SDL2_Build
-    make;
-    make install;
-    cd /home/travis/build/JPEGtheDev/Particle-Viewer;
+hg clone https://hg.libsdl.org/SDL SDL;
+cd SDL;
+mkdir build;
+cd build;
+../configure --prefix=/home/travis/build/JPEGtheDev/SDL2_Build
+make;
+make install;
+cd /home/travis/build/JPEGtheDev/Particle-Viewer;
