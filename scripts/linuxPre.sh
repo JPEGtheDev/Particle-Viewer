95e35fd76dd05c05a420432b8ebb350db71f9af2
Jonathan Petz <JPEGtheDev@users.noreply.github.com>
2015-09-28 01:17:31 -0500

    Update linuxPre.sh
    checking to see if spacing makes a difference

--- scripts/linuxPre.sh
+++ scripts/linuxPre.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 touch /home/travis/build/JPEGtheDev/SDL2_Build;
 ls /home/travis/build/JPEGtheDev/SDL2_Build;
-if [ !"$(ls -A /home/travis/build/JPEGtheDev/SDL2_Build)" ]; then
+if [ ! "$(ls -A /home/travis/build/JPEGtheDev/SDL2_Build)" ]; then
 	hg clone https://hg.libsdl.org/SDL SDL;
 	cd SDL;
 	mkdir build;
