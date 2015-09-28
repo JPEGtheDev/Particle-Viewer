39c4a62c8577afadb3a4feeb76f959c9996f6cf8
Jonathan Petz <JPEGtheDev@users.noreply.github.com>
2015-09-28 01:08:33 -0500

    Update linuxPre.sh
    I hate spaces over tabs

--- scripts/linuxPre.sh
+++ scripts/linuxPre.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env sh
 touch /home/travis/build/JPEGtheDev/SDL2_Build
 if [ !"$(ls -A /home/travis/build/JPEGtheDev/SDL2_Build)" ]; then
-    hg clone https://hg.libsdl.org/SDL SDL;
-    cd SDL;
-    mkdir build;
-    cd build;
-    ../configure --prefix=/home/travis/build/JPEGtheDev/SDL2_Build
-    make;
-    make install;
-    cd /home/travis/build/JPEGtheDev/Particle-Viewer;
+	hg clone https://hg.libsdl.org/SDL SDL;
+	cd SDL;
+	mkdir build;
+	cd build;
+	../configure --prefix=/home/travis/build/JPEGtheDev/SDL2_Build
+	make;
+	make install;
+	cd /home/travis/build/JPEGtheDev/Particle-Viewer;
 fi
