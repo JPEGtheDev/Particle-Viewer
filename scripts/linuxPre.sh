8df184180f037fded9ef6f0149357c1f1e573973
Jonathan Petz <JPEGtheDev@users.noreply.github.com>
2015-09-28 00:31:37 -0500

    Update linuxPre.sh
    testing to see if I can properly cache assets

--- scripts/linuxPre.sh
+++ scripts/linuxPre.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env sh
-hg clone https://hg.libsdl.org/SDL SDL;
-cd SDL;
-mkdir build;
-cd build;
-../configure --prefix=/home/travis/build/JPEGtheDev/SDL2_Build
-make;
-make install;
-cd /home/travis/build/JPEGtheDev/Particle-Viewer;
+if [ ! -d "/home/travis/build/JPEGtheDev/SDL2_Build" ]; then
+  hg clone https://hg.libsdl.org/SDL SDL;
+  cd SDL;
+  mkdir build;
+  cd build;
+  ../configure --prefix=/home/travis/build/JPEGtheDev/SDL2_Build
+  make;
+  make install;
+  cd /home/travis/build/JPEGtheDev/Particle-Viewer;
+fi
