4fbb9510b43d263c0ff8de7f5802d8869764530a
Jonathan Petz <JPEGtheDev@users.noreply.github.com>
2015-09-28 00:45:36 -0500

    Update linuxPre.sh
    lets see if the directory exists then check to see if its empty

--- scripts/linuxPre.sh
+++ scripts/linuxPre.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env sh
-
-if [ $(find "/home/travis/build/JPEGtheDev/SDL2_Build" -maxdepth 0 -type d -empty 2>/dev/null)]; then
-  hg clone https://hg.libsdl.org/SDL SDL;
-  cd SDL;
-  mkdir build;
-  cd build;
-  ../configure --prefix=/home/travis/build/JPEGtheDev/SDL2_Build
-  make;
-  make install;
-  cd /home/travis/build/JPEGtheDev/Particle-Viewer;
+if [ ! -d "/home/travis/build/JPEGtheDev/SDL2_Build" ]; then
+  if [ $(find "/home/travis/build/JPEGtheDev/SDL2_Build" -maxdepth 0 -type d -empty 2>/dev/null)]; then
+    hg clone https://hg.libsdl.org/SDL SDL;
+    cd SDL;
+    mkdir build;
+    cd build;
+    ../configure --prefix=/home/travis/build/JPEGtheDev/SDL2_Build
+    make;
+    make install;
+    cd /home/travis/build/JPEGtheDev/Particle-Viewer;
+    fi
 fi
