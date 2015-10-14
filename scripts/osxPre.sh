4f2e486e25879ebd90dc617c7d19ad61441cb3ab
Jonathan Petz <JPEGtheDev@users.noreply.github.com>
2015-10-14 12:28:34 -0500

    Update osxPre.sh

--- scripts/osxPre.sh
+++ scripts/osxPre.sh
@@ -1,5 +1,5 @@
 if [ ! "$(ls -A /User/travis/build/JPEGtheDev/glm)" ]; then
-	
+	pwd;
 	cd /User/travis/build/JPEGtheDev/;
 	git clone https://github.com/g-truc/glm.git;
 	cd /User/travis/build/JPEGtheDev/Particle-Viewer;
