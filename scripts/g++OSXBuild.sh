3a7e8a180933ac8c5079cf7c0bf2cf98574952aa
Jonathan Petz <JPEGtheDev@users.noreply.github.com>
2015-10-14 13:31:47 -0500

    Update g++OSXBuild.sh

--- scripts/g++OSXBuild.sh
+++ scripts/g++OSXBuild.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 #gcc build script
-g++ ./src/main.cpp -o ./bin/viewer -ldl -pthread  -I. -I/Users/travis/build/JPEGtheDev/glm -g -O0 -lGLEW -lSDL2;
+g++ ./src/main.cpp -o ./bin/viewer -I. -I/Users/travis/build/JPEGtheDev/glm -g -O0 -lGLEW -lSDL2;
