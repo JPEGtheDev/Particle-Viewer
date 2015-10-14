f69676ddc7cf169af2c8f848ad560ab6d7633bbe
Jonathan Petz <JPEGtheDev@users.noreply.github.com>
2015-10-14 13:48:21 -0500

    Update g++LinuxBuild.sh

--- scripts/g++LinuxBuild.sh
+++ scripts/g++LinuxBuild.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 #gcc build script
-g++ ./src/main.cpp -o ./bin/viewer -ldl -pthread  -I. -g -O0  -L/home/travis/build/JPEGtheDev/SDL2_Build/lib/ -I/home/travis/build/JPEGtheDev/SDL2_Build/include -I/home/travis/build/JPEGtheDev/glm/glm  -lGLEW -lSDL2;
+g++ ./src/main.cpp -o ./bin/viewer -ldl -pthread  -I. -g -O0  -L/home/travis/build/JPEGtheDev/SDL2_Build/lib/ -I/home/travis/build/JPEGtheDev/SDL2_Build/include -I/home/travis/build/JPEGtheDev/glm  -lGLEW -lSDL2;
