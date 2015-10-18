b5325289d9d9cb2938032139a12e136f97295e1c
Jonathan Petz <JPEGtheDev@gmail.com>
2015-10-18 12:50:57 -0400

    Implemented a cmake system, the only exception to the whole cmake thing is assimp

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 
 //Generally Accepted Includes
 #include <SDL2/SDL.h>
-#include <GL/glew.h>
+#include "glad/glad.h"
 #include <glm/glm.hpp>
 
 //OS Specific Includes
@@ -20,6 +20,7 @@ int main(int argc, char **argv)
 {
 	SDL_Init(SDL_INIT_EVERYTHING);
 	SDL_Delay(1000);
+	std::cout << "Hello World" << std::endl;
 	SDL_Quit();
 	return 0;
 }
