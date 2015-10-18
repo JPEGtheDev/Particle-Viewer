9f2bb1acdaf9d7627cb84053a6c8911676e2ee8e
Jonathan Petz <JPEGtheDev@gmail.com>
2015-10-18 13:11:58 -0500

    I also forgot to mention that there is no longer a strict requirement for which ide that you need to use. This is just a plain old cmake based system. I also modded the build script for linux to use the new cmake system.

--- scripts/g++LinuxBuild.sh
+++ scripts/g++LinuxBuild.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env sh
 #gcc build script
-g++ ./src/main.cpp -o ./bin/viewer -I. -g -O0  -L/home/travis/build/JPEGtheDev/SDL2_Build/lib/ -L/home/travis/build/JPEGtheDev/assimp/lib -I/home/travis/build/JPEGtheDev/SDL2_Build/include -I/home/travis/build/JPEGtheDev/glm/include  -I/home/travis/build/JPEGtheDev/assimp/include -lGLEW -lSDL2 -lassimp;
+mkdir build;
+cd build;
+cmake ..;
+make;
+#g++ ./src/main.cpp -o ./bin/viewer -I. -g -O0  -L/home/travis/build/JPEGtheDev/SDL2_Build/lib/ -L/home/travis/build/JPEGtheDev/assimp/lib -I/home/travis/build/JPEGtheDev/SDL2_Build/include -I/home/travis/build/JPEGtheDev/glm/include  -I/home/travis/build/JPEGtheDev/assimp/include -lGLEW -lSDL2 -lassimp;
