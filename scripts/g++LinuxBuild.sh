54aaad24cfdf34165946731e0b2d5e37b7be1951
Jonathan Petz <JPEGtheDev@gmail.com>
2015-10-18 13:33:44 -0500

    modded some scripts to test travis build stuff

--- scripts/g++LinuxBuild.sh
+++ scripts/g++LinuxBuild.sh
@@ -2,6 +2,6 @@
 #gcc build script
 mkdir build;
 cd build;
-cmake ..;
+cmake .. -DGLM_INCLUDE_DIR=/home/travis/build/JPEGtheDev/glm/;
 make;
 #g++ ./src/main.cpp -o ./bin/viewer -I. -g -O0  -L/home/travis/build/JPEGtheDev/SDL2_Build/lib/ -L/home/travis/build/JPEGtheDev/assimp/lib -I/home/travis/build/JPEGtheDev/SDL2_Build/include -I/home/travis/build/JPEGtheDev/glm/include  -I/home/travis/build/JPEGtheDev/assimp/include -lGLEW -lSDL2 -lassimp;
--- scripts/osxPre.sh
+++ scripts/osxPre.sh
@@ -1,9 +1,1 @@
-if [ ! "$(ls -A /Users/travis/build/JPEGtheDev/glm)" ]; then
-	pwd;
-	cd /Users/travis/build/JPEGtheDev/;
-	git clone https://github.com/g-truc/glm.git;
-	cd glm;
-	cmake .;
-	make install;
-	cd /Users/travis/build/JPEGtheDev/Particle-Viewer;
-fi
+brew update && brew install sdl2 glew glm assimp;
