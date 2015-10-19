7d2d1d1a54d0aa2eb64a64fc1cce11184213ae62
Jonathan Petz <JPEGtheDev@users.noreply.github.com>
2015-10-19 01:30:25 -0500

    Update linuxPre.sh

--- scripts/linuxPre.sh
+++ scripts/linuxPre.sh
@@ -57,7 +57,8 @@ if [ ! "$(ls -A /home/travis/build/JPEGtheDev/cmake)" ]; then
 	mkdir cmakeBuild;
 	cd cmakeBuild;
 	wget --no-check-certificate "https://cmake.org/files/v3.3/cmake-3.3.2-Linux-x86_64.sh" ;
+	mkdir /home/travis/build/JPEGtheDev/cmake;
+	chmod 777 -R /home/travis/build/JPEGtheDev/cmake;
 	sh ./cmake-3.3.2-Linux-x86_64.sh --prefix=/home/travis/build/JPEGtheDev/cmake --exclude-subdir;
 	cd /home/travis/build/JPEGtheDev/Particle-Viewer;
 fi
-ls /home/travis/build/JPEGtheDev/SDL2_Build/lib
