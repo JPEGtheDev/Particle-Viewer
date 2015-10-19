b4aae578ca2b351df607daa76dc20e044b0a0177
Jonathan Petz <JPEGtheDev@users.noreply.github.com>
2015-10-19 01:19:13 -0500

    Update linuxPre.sh

--- scripts/linuxPre.sh
+++ scripts/linuxPre.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
-touch /home/travis/build/JPEGtheDev/SDL2_Build;
+#touch /home/travis/build/JPEGtheDev/SDL2_Build;
 
 if [ ! "$(ls -A /home/travis/build/JPEGtheDev/SDL2_Build)" ]; then
 	hg clone https://hg.libsdl.org/SDL SDL;
