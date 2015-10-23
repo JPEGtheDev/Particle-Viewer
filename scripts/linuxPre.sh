e911b5cbf8c8ef200b1972fff4f1d3e2e37f83be
Jonathan Petz <jpegthedev@gmail.com>
2015-10-22 19:46:01 -0500

    updated the build to compile libRocket, doing this on the road so it might break.

--- scripts/linuxPre.sh
+++ scripts/linuxPre.sh
@@ -54,11 +54,23 @@ fi
 if [ ! "$(ls -A /home/travis/build/JPEGtheDev/cmake)" ]; then
 	
 	cd /home/travis/build/JPEGtheDev/;
-	mkdir cmakeBuild;
+		mkdir cmakeBuild;
 	cd cmakeBuild;
 	wget --no-check-certificate "https://cmake.org/files/v3.3/cmake-3.3.2-Linux-x86_64.sh" ;
 	mkdir /home/travis/build/JPEGtheDev/cmake;
 	chmod 777 -R /home/travis/build/JPEGtheDev/cmake;
 	sh ./cmake-3.3.2-Linux-x86_64.sh --prefix=/home/travis/build/JPEGtheDev/cmake --exclude-subdir;
+	cd /home/travis/build/JPEGtheDev/Particle-Viewer;
+fi
+
+if [ ! "$(ls -A /home/travis/build/JPEGtheDev/libRocket)" ]; then
+	cd /home/travis/build/JPEGtheDev/;
+	mkdir rocketBuild;
+	cd rocketBuild;
+	git clone https://github.com/libRocket/libRocket.git;
+	cd libRocket;
+	cmake -DCMAKE_INSTALL_PREFIX=/home/travis/build/JPEGtheDev/libRocket .;
+	make;
+	make install;
 	cd /home/travis/build/JPEGtheDev/Particle-Viewer;
 fi
