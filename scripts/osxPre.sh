457e1af56e55a7034dbe1f32b96a59ac7f6df35f
Jonathan Petz <JPEGtheDev@users.noreply.github.com>
2015-10-23 15:17:34 -0500

    Update osxPre.sh

--- scripts/osxPre.sh
+++ scripts/osxPre.sh
@@ -1,1 +1,14 @@
 brew update && brew install sdl2 glew glm assimp;
+if [ ! "$(ls -A /home/travis/build/JPEGtheDev/libRocket)" ]; then
+	cd /home/travis/build/JPEGtheDev/;
+	mkdir rocketBuild;
+	cd rocketBuild;
+	git clone https://github.com/libRocket/libRocket.git;
+	cd libRocket
+	sh ./Dependencies/osx-depends.sh;
+	cd Build;
+	cmake -DCMAKE_INSTALL_PREFIX=/home/travis/build/JPEGtheDev/libRocket .;
+	make;
+	make install;
+	cd /home/travis/build/JPEGtheDev/Particle-Viewer;
+fi
