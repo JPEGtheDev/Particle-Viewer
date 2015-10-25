6d7a92abf233e588505624ec628245cb7800c5a3
Jonathan Petz <JPEGtheDev@users.noreply.github.com>
2015-10-25 00:49:05 -0500

    Update osxPre.sh

--- scripts/osxPre.sh
+++ scripts/osxPre.sh
@@ -1,5 +1,10 @@
 brew update && brew install sdl2 glew glm assimp boost-python;
+
 if [ ! "$(ls -A /Users/travis/build/JPEGtheDev/libRocket)" ]; then
+	BUILD_FREETYPE2=YES
+	BUILD_BOOST_PYTHON=YES
+	BUILD_LUA=YES
+	BUILD_PLATFORM=osx
 	cd /Users/travis/build/JPEGtheDev/;
 	mkdir rocketBuild;
 	cd rocketBuild;
