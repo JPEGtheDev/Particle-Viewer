8bf6ac4cbdf656fd962a17acc489421cb9767b3a
Jonathan Petz <JPEGtheDev@users.noreply.github.com>
2015-10-25 01:17:05 -0500

    Update osxPre.sh

--- scripts/osxPre.sh
+++ scripts/osxPre.sh
@@ -1,18 +1,12 @@
 brew update && brew install sdl2 glew glm assimp boost-python;
 
 if [ ! "$(ls -A /Users/travis/build/JPEGtheDev/libRocket)" ]; then
-	BUILD_FREETYPE2=YES
-	BUILD_BOOST_PYTHON=YES
-	BUILD_LUA=YES
-	BUILD_PLATFORM=osx
 	cd /Users/travis/build/JPEGtheDev/;
 	mkdir rocketBuild;
 	cd rocketBuild;
 	git clone https://github.com/libRocket/libRocket.git;
 	cd libRocket
-	#sh ./Dependencies/osx-depends.sh;
 	cd Build;
-	cmake -LAH;
 	cmake .;
 	make;
 	sudo make install;
