922442a38fa7e60e9b5435376c8dc1bf9e1b1510
Jonathan Petz <JPEGtheDev@users.noreply.github.com>
2015-11-26 23:22:04 -0600

    trying SDL2 with cmake
    hopefully this build version will work

--- scripts/linuxPre.sh
+++ scripts/linuxPre.sh
@@ -3,24 +3,19 @@
 
 if [ ! "$(ls -A /home/travis/build/JPEGtheDev/SDL2_Build)" ]; then
 	hg clone https://hg.libsdl.org/SDL SDL;
-	cd SDL;
+	pushd SDL;
 	mkdir build;
 	cd build;
-	if [ "$(id -u)" != "0" ]; then
-	 ../configure --prefix=/home/travis/build/JPEGtheDev/SDL2_Build;
-	else
-	 ../configure
-	fi
-	
+	cmake .. -DCMAKE_INSTALL_PREFIX="/home/travis/build/JPEGtheDev/SDL2_Build";
 	make;
 	make install;
-	cd /home/travis/build/JPEGtheDev/Particle-Viewer;
+	popd;
 fi
 
 if [ ! "$(ls -A /home/travis/build/JPEGtheDev/cmake)" ]; then
 	
 	cd /home/travis/build/JPEGtheDev/;
-		mkdir cmakeBuild;
+	mkdir cmakeBuild;
 	cd cmakeBuild;
 	wget --no-check-certificate "https://cmake.org/files/v3.3/cmake-3.3.2-Linux-x86_64.sh" ;
 	mkdir /home/travis/build/JPEGtheDev/cmake;
