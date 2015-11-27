2b3f93c4a858c569d1aa27ff13e9575eab214afe
Jonathan Petz <JPEGtheDev@users.noreply.github.com>
2015-11-26 23:58:39 -0600

    Update linuxBuild.sh
    reverting, that broke it even more

--- scripts/linuxBuild.sh
+++ scripts/linuxBuild.sh
@@ -2,5 +2,4 @@
 #gcc build script
 mkdir build;
 cd build;
-/home/travis/build/JPEGtheDev/cmake/bin/cmake ..  -DSDL2_INCLUDE_DIR=/home/travis/build/JPEGtheDev/SDL2_Build/include -DSDL2_LIBRARY=/home/travis/build/JPEGtheDev/SDL2_Build/lib/libSDL2.so -DSDL2MAIN_LIBRARY=/home/travis/build/JPEGtheDev/SDL2_Build/lib/libSDL2main.a -DCMAKE_CXX_FLAGS="-stdlib=libc++"
-make;
+/home/travis/build/JPEGtheDev/cmake/bin/cmake ..  -DSDL2_INCLUDE_DIR=/home/travis/build/JPEGtheDev/SDL2_Build/include -DSDL2_LIBRARY=/home/travis/build/JPEGtheDev/SDL2_Build/lib/libSDL2.so -DSDL2MAIN_LIBRARY=/home/travis/build/JPEGtheDev/SDL2_Build/lib/libSDL2main.a
