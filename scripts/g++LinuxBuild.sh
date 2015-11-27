82760a95db8a040f7c7398af6ea43a373fbd603d
Jonathan Petz <jpegthedev@gmail.com>
2015-11-26 23:48:58 -0600

    renamed and updated
    trying to get the build to work properly for clang and gcc/g++

--- scripts/g++LinuxBuild.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-#gcc build script
-mkdir build;
-cd build;
-/home/travis/build/JPEGtheDev/cmake/bin/cmake ..  -DSDL2_INCLUDE_DIR=/home/travis/build/JPEGtheDev/SDL2_Build/include/SDL2 -DSDL2_LIBRARY=/home/travis/build/JPEGtheDev/SDL2_Build/lib/libSDL2.so -DSDL2MAIN_LIBRARY=/home/travis/build/JPEGtheDev/SDL2_Build/lib/libSDL2main.a
-make;
---
+++ scripts/linuxBuild.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+#gcc build script
+mkdir build;
+cd build;
+/home/travis/build/JPEGtheDev/cmake/bin/cmake ..  -DSDL2_INCLUDE_DIR=/home/travis/build/JPEGtheDev/SDL2_Build/include -DSDL2_LIBRARY=/home/travis/build/JPEGtheDev/SDL2_Build/lib/libSDL2.so -DSDL2MAIN_LIBRARY=/home/travis/build/JPEGtheDev/SDL2_Build/lib/libSDL2main.a
+make;
