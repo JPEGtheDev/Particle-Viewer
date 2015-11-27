4410404f77be0fbf2b32c292b79d032bf89d849c
Jonathan Petz <JPEGtheDev@users.noreply.github.com>
2015-11-26 23:26:19 -0600

    Update linuxPre.sh
    lets try this again...

--- scripts/linuxPre.sh
+++ scripts/linuxPre.sh
@@ -1,25 +1,26 @@
 #!/usr/bin/env sh
 #touch /home/travis/build/JPEGtheDev/SDL2_Build;
-
-if [ ! "$(ls -A /home/travis/build/JPEGtheDev/SDL2_Build)" ]; then
-	hg clone https://hg.libsdl.org/SDL SDL;
-	pushd SDL;
-	mkdir build;
-	cd build;
-	cmake .. -DCMAKE_INSTALL_PREFIX="/home/travis/build/JPEGtheDev/SDL2_Build";
-	make;
-	make install;
-	popd;
-fi
-
 if [ ! "$(ls -A /home/travis/build/JPEGtheDev/cmake)" ]; then
 	
-	cd /home/travis/build/JPEGtheDev/;
+	pushd /home/travis/build/JPEGtheDev/;
 	mkdir cmakeBuild;
 	cd cmakeBuild;
 	wget --no-check-certificate "https://cmake.org/files/v3.3/cmake-3.3.2-Linux-x86_64.sh" ;
 	mkdir /home/travis/build/JPEGtheDev/cmake;
 	chmod 777 -R /home/travis/build/JPEGtheDev/cmake;
 	sh ./cmake-3.3.2-Linux-x86_64.sh --prefix=/home/travis/build/JPEGtheDev/cmake --exclude-subdir;
-	cd /home/travis/build/JPEGtheDev/Particle-Viewer;
+	popd;
 fi
+
+if [ ! "$(ls -A /home/travis/build/JPEGtheDev/SDL2_Build)" ]; then
+	hg clone https://hg.libsdl.org/SDL SDL;
+	pushd SDL;
+	mkdir build;
+	cd build;
+	/home/travis/build/JPEGtheDev/cmake/bin/cmake .. -DCMAKE_INSTALL_PREFIX="/home/travis/build/JPEGtheDev/SDL2_Build";
+	make;
+	make install;
+	popd;
+fi
+
+
