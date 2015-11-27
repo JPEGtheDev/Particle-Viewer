13d1c73127a89ac37cdeff58179b9dcea6d12934
Jonathan Petz <JPEGtheDev@users.noreply.github.com>
2015-11-26 23:56:40 -0600

    Update linuxBuild.sh
    trying to fix clang

--- scripts/linuxBuild.sh
+++ scripts/linuxBuild.sh
@@ -2,5 +2,5 @@
 #gcc build script
 mkdir build;
 cd build;
-/home/travis/build/JPEGtheDev/cmake/bin/cmake ..  -DSDL2_INCLUDE_DIR=/home/travis/build/JPEGtheDev/SDL2_Build/include -DSDL2_LIBRARY=/home/travis/build/JPEGtheDev/SDL2_Build/lib/libSDL2.so -DSDL2MAIN_LIBRARY=/home/travis/build/JPEGtheDev/SDL2_Build/lib/libSDL2main.a
+/home/travis/build/JPEGtheDev/cmake/bin/cmake ..  -DSDL2_INCLUDE_DIR=/home/travis/build/JPEGtheDev/SDL2_Build/include -DSDL2_LIBRARY=/home/travis/build/JPEGtheDev/SDL2_Build/lib/libSDL2.so -DSDL2MAIN_LIBRARY=/home/travis/build/JPEGtheDev/SDL2_Build/lib/libSDL2main.a -DCMAKE_CXX_FLAGS="-stdlib=libc++"
 make;
