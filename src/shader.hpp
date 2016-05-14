d7f7a7c03c92aea6cadf2462b05c97c4ea655c16
Jonathan Petz <JPEG@stark.westeros.lan>
2016-05-14 14:20:00 -0500

    This is viewer cleanup part 1. I plan on cleaning out some extraneous code and tuning up the application a bit. Things I plan to do over the next couple of weeks: Move "untouched" classes to header only libraries, as they are supposed to not be touched at all. I'm going to TRY to convince a group of people to move over to JSON for the "settings" file, in order to remove 10(!) pages of code that is used for declaration and base casing. JSON would also allow for us to reduce the amount of text data that we need to parse. I also plan on adding in buffer syncing, in order to not have to constantly delete the Vertex Buffer.

--- src/shader.hpp
+++ src/shader.hpp
@@ -14,11 +14,52 @@ class Shader
 public:
     GLuint Program;
     // Constructor generates the shader on the fly
-    Shader();
-    Shader(const GLchar* vertexPath, const GLchar* fragmentPath);
-    
-    // Uses the current shader
-    void Use();
+    Shader(const GLchar* vShaderCode, const GLchar* fShaderCode)
+    {
+        GLuint vertex, fragment;
+        GLint success;
+        GLchar infoLog[512];
+        // Vertex Shader
+        vertex = glCreateShader(GL_VERTEX_SHADER);
+        glShaderSource(vertex, 1, &vShaderCode, NULL);
+        glCompileShader(vertex);
+        // Print compile errors if any
+        glGetShaderiv(vertex, GL_COMPILE_STATUS, &success);
+        if (!success)
+        {
+            glGetShaderInfoLog(vertex, 512, NULL, infoLog);
+            std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+        }
+        // Fragment Shader
+        fragment = glCreateShader(GL_FRAGMENT_SHADER);
+        glShaderSource(fragment, 1, &fShaderCode, NULL);
+        glCompileShader(fragment);
+        // Print compile errors if any
+        glGetShaderiv(fragment, GL_COMPILE_STATUS, &success);
+        if (!success)
+        {
+            glGetShaderInfoLog(fragment, 512, NULL, infoLog);
+            std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+        }
+        // Shader Program
+        this->Program = glCreateProgram();
+        glAttachShader(this->Program, vertex);
+        glAttachShader(this->Program, fragment);
+        glLinkProgram(this->Program);
+        // Print linking errors if any
+        glGetProgramiv(this->Program, GL_LINK_STATUS, &success);
+        if (!success)
+        {
+            glGetProgramInfoLog(this->Program, 512, NULL, infoLog);
+            std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+        }
+        // Delete the shaders as they're linked into our program now and no longer necessery
+        glDeleteShader(vertex);
+        glDeleteShader(fragment);
+
+    };
+    Shader(){};
+    void Use(){glUseProgram(this->Program);};
 };
 
 #endif
