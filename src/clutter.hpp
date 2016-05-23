ff2a71f1192319f61044fc8c44d524f9922ec499
Jonathan Petz <jpegthedev@gmail.com>
2016-05-23 14:51:20 -0500

    Killed something
    For some reason removing this killed linux support. I need to
    investigate further, but I’m leaving this in for now.

--- src/clutter.hpp
+++ src/clutter.hpp
@@ -85,6 +85,8 @@
 	  SDL_GL_LoadLibrary(NULL);
 	  
 	  //set base GL stuff
+	  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4); //Don't remove, killed running on linux
+	  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0); //Don't remove, killed running on linux
 	  SDL_GL_SetAttribute(SDL_GL_ACCELERATED_VISUAL, 1);
 	  SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
 	  SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
